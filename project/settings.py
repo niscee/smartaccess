be3c62952d4e2390a390f43caec81b0369c4f35a
rananischal15@gmail.com <rananischal15@gmail.com>
2021-09-03 18:21:16 +1000

    static root

--- project/settings.py
+++ project/settings.py
@@ -141,11 +141,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static_files')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 # for media content.
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
 
 
